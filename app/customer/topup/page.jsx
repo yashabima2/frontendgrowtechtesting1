ba6de0987f0369e2a0ad19e7ec3345dced5683fb
yashabima2 <rivandio81@gmail.com>
2026-02-17 17:17:03 +0700

    "rev208"

--- app/customer/topup/page.jsx
+++ app/customer/topup/page.jsx
@@ -175,7 +175,7 @@ export default function TopUpPage() {
 
           <Card>
             <Header title="Saldo Wallet Anda" icon={
-              <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" viewBox="0 0 24 24"><g fill="none" stroke="#9333ea" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M17 8V5a1 1 0 0 0-1-1H6a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1-1 1H6a2 2 0 0 1-2-2V6"/><path d="M20 12v4h-4a2 2 0 0 1 0-4z"/></g></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><g fill="none" stroke="#9333ea" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M17 8V5a1 1 0 0 0-1-1H6a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1-1 1H6a2 2 0 0 1-2-2V6"/><path d="M20 12v4h-4a2 2 0 0 1 0-4z"/></g></svg>
             } />
             <p className="text-sm text-gray-400">Total Saldo</p>
             <p className="text-3xl font-bold mt-2 mb-2">
