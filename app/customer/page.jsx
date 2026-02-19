da405963eed8095accdbaa3ae824b1df4f7acf31
yashabima2 <rivandio81@gmail.com>
2026-02-19 11:24:52 +0700

    "rev214"

--- app/customer/page.jsx
+++ app/customer/page.jsx
@@ -1,7 +1,5 @@
-export const dynamic = "force-dynamic";
-
 'use client'
-
+export const dynamic = "force-dynamic";
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
